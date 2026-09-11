49e97b0c465242321a6cd43dc54626deebf64d90
riddhimithastalin <riddhimithastalin@gmail.com>
2026-09-12 00:27:06 +0530

    Add reverse customer name problem

---
+++ src/main/java/string/class_problems/ReverseCustomerName.java
@@ -0,0 +1,32 @@
+package string.class_problems;
+
+import java.util.Scanner;
+
+public class ReverseCustomerName {
+
+    public static String reverseCustomerName(String customerName) {
+
+        String reversed = "";
+
+        for (int i = customerName.length() - 1; i >= 0; i--) {
+            reversed += customerName.charAt(i);
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = sc.nextLine();
+
+        String reversed = reverseCustomerName(customerName);
+
+        System.out.println("Original: " + customerName);
+        System.out.println("Reversed: " + reversed);
+
+        sc.close();
+    }
+}
