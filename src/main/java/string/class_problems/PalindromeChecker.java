8a41a66928c7f56600bbb5d1a169af9a1e164eb8
riddhimithastalin <riddhimithastalin@gmail.com>
2026-09-12 00:21:57 +0530

    Add palindrome checker problem

---
+++ src/main/java/string/class_problems/PalindromeChecker.java
@@ -0,0 +1,69 @@
+package string.class_problems;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static boolean isPalindromeIterative(String text) {
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindromeRecursive(String text) {
+        return checkRecursive(text, 0, text.length() - 1);
+    }
+
+    public static boolean checkRecursive(String text, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+
+        if (text.charAt(left) != text.charAt(right)) {
+            return false;
+        }
+
+        return checkRecursive(text, left + 1, right - 1);
+    }
+
+    public static boolean isPalindromeArrayReversal(String text) {
+        char[] original = text.toCharArray();
+        char[] reversed = new char[original.length];
+
+        for (int i = 0; i < original.length; i++) {
+            reversed[i] = original[original.length - 1 - i];
+        }
+
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] != reversed[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String text = sc.next();
+
+        System.out.println("Iterative: " + isPalindromeIterative(text));
+        System.out.println("Recursive: " + isPalindromeRecursive(text));
+        System.out.println("Array Reversal: " + isPalindromeArrayReversal(text));
+
+        sc.close();
+    }
+}
