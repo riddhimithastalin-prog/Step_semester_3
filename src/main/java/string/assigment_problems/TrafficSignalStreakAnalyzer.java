e74e1c5f468e5da831393ec6e91f6fe0cef698af
riddhimithastalin <riddhimithastalin@gmail.com>
2026-09-12 09:12:22 +0530

    Add traffic signal streak analyzer

---
+++ src/main/java/string/assigment_problems/TrafficSignalStreakAnalyzer.java
@@ -0,0 +1,46 @@
+package string.assigment_problems;
+
+import java.util.Scanner;
+
+public class TrafficSignalStreakAnalyzer {
+
+    public static void findLongestStreak(String signalLog) {
+
+        int longestStreak = 1;
+        int currentStreak = 1;
+        char longestColor = signalLog.charAt(0);
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                currentStreak++;
+            } else {
+                currentStreak = 1;
+            }
+
+            if (currentStreak > longestStreak) {
+                longestStreak = currentStreak;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestColor
+                + "' repeated " + longestStreak + " times");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter signal log: ");
+        String signalLog = sc.nextLine();
+
+        if (signalLog.isEmpty()) {
+            System.out.println("Error: Signal log cannot be empty.");
+        } else {
+            findLongestStreak(signalLog);
+        }
+
+        sc.close();
+    }
+}
